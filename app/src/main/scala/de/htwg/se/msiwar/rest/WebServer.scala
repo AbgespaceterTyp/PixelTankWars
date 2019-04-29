2254beefd17d29b892105294164859f594a9a37e
Lukas Groß <lu851gro@htwg-konstanz.de>
2019-04-29 14:31:52 +0200

    moved routes in own file, added routes for cell information

--- app/src/main/scala/de/htwg/se/msiwar/rest/WebServer.scala
+++ app/src/main/scala/de/htwg/se/msiwar/rest/WebServer.scala
@@ -3,10 +3,7 @@ package de.htwg.se.msiwar.rest
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
-import de.htwg.se.msiwar.aview.MainApp
-import de.htwg.se.msiwar.util.JsonConverter
 
 import scala.io.StdIn
 
@@ -19,58 +16,7 @@ object WebServer {
     // needed for the future flatMap/onComplete in the end
     implicit val executionContext = system.dispatcher
 
-    val route =
-      get {
-        pathPrefix("commands" / LongNumber) { line =>
-          complete {
-            "" + MainApp.tui.executeCommand(line.toString)
-          }
-        } ~
-        pathPrefix("actions") {
-          path("check"/ IntNumber / IntNumber / IntNumber) { (actionId, rowIndex, columnIndex) =>
-            complete {
-              "" + MainApp.controller.canExecuteAction(actionId, rowIndex, columnIndex)
-            }
-          } ~
-          path("cost"/ IntNumber) { (actionId) =>
-            complete {
-              "" + MainApp.controller.actionPointCost(actionId)
-            }
-          } ~
-          path("desc"/ IntNumber) { (actionId) =>
-            complete {
-              "" + MainApp.controller.actionDescription(actionId)
-            }
-          } ~
-          path("icon"/ IntNumber) { (actionId) =>
-            complete {
-              "" + MainApp.controller.actionIconPath(actionId)
-            }
-          } ~
-          path("damage"/ IntNumber) { (actionId) =>
-            complete {
-              "" + MainApp.controller.actionDamage(actionId)
-            }
-          } ~
-          path("range"/ IntNumber) { (actionId) =>
-            complete {
-              "" + MainApp.controller.actionRange(actionId)
-            }
-          }
-        }
-      } ~
-      put {
-        pathPrefix("actions") {
-          path("execute" / IntNumber / IntNumber / IntNumber) { (actionId, rowIndex, columnIndex) =>
-            complete {
-              MainApp.controller.executeAction(actionId, rowIndex, columnIndex)
-              JsonConverter.gameBoardToJson().toString()
-            }
-          }
-        }
-      }
-
-    val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+    val bindingFuture = Http().bindAndHandle(Routes.all, "localhost", 8080)
     println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
     StdIn.readLine() // let it run until user presses return
     bindingFuture
