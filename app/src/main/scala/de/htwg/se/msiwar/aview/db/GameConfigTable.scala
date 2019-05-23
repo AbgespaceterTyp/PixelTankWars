2e7b2bae330f2e700ae7b2f09848f3301245ddb0
Lukas Groß <lu851gro@htwg-konstanz.de>
2019-05-23 13:32:15 +0200

    GameConfigTable: added simple fields for game configuration, list of game objects missing

--- app/src/main/scala/de/htwg/se/msiwar/aview/db/GameConfigTable.scala
+++ app/src/main/scala/de/htwg/se/msiwar/aview/db/GameConfigTable.scala
@@ -2,15 +2,26 @@ package de.htwg.se.msiwar.aview.db
 
 import slick.driver.H2Driver.api._
 
-case class GameConfig(name: String, id: Option[Int] = None) {}
+case class GameConfig(attackSoundPath: String, openingBackgroundImagePath: String, levelBackgroundImagePath: String,
+                      actionbarBackgroundImagePath:String, attackImagePath: String, appIconImagePath: String,
+                      rowCount: Int, colCount: Int, id: Option[Int] = None)
 
 class GameConfigTable(tag: Tag) extends Table[GameConfig](tag, "GAMECONFIGS") {
     // Auto Increment the id primary key column
     def id = column[Int]("ID", O.PrimaryKey, O.AutoInc)
-    // The name can't be null
-    def name = column[String]("NAME")
+    def attackSoundPath = column[String]("ATT_SOUND_PATH")
+    def openingBackgroundImagePath = column[String]("OP_IMG_PATH")
+    def levelBackgroundImagePath = column[String]("LVL_IMG_PATH")
+    def actionbarBackgroundImagePath = column[String]("ACT_IMG_PATH")
+    def attackImagePath = column[String]("ATT_IMG_PATH")
+    def appIconImagePath = column[String]("APP_IMG_PATH")
+    def rowCount = column[Int]("ROW_CNT")
+    def colCount = column[Int]("COL_CNT")
+
     // the * projection (e.g. select * ...) auto-transforms the tupled
-    // column values to / from a GameConfig
+    //    // column values to / from a GameConfig
     // Every table needs a * projection with the same type as the table's type parameter
-    def * = (name, id.?) <> (GameConfig.tupled, GameConfig.unapply)
+    def * = (attackSoundPath, openingBackgroundImagePath, levelBackgroundImagePath,
+      actionbarBackgroundImagePath, attackImagePath, appIconImagePath,
+      rowCount, colCount, id.?) <> (GameConfig.tupled, GameConfig.unapply)
 }
